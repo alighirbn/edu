ea4f2ae943a8f0057d985365b4dd158a6103bafe
alighirbn <alighirbn@gmail.com>
2024-03-29 21:45:29 +0300

    fix

--- database/seeders/Job_Title_Seeder.php
+++ database/seeders/Job_Title_Seeder.php
@@ -18,42 +18,42 @@ class Job_Title_Seeder extends Seeder
       [
         'job_title' => 'غير محدد',
         'next_job_title_id' => 1,
-        'career_stage_id' => 1
+        'job_grade_id' => 1
       ],
       [
         'job_title' => 'مستشار',
         'next_job_title_id' => null,
-        'career_stage_id' => 1
+        'job_grade_id' => 1
       ],
       [
         'job_title' => 'مساعد مستشار',
         'next_job_title_id' => 2,
-        'career_stage_id' => 2
+        'job_grade_id' => 2
       ],
       [
         'job_title' => 'مشاور قانوني اقدم',
         'next_job_title_id' => 3,
-        'career_stage_id' => 3
+        'job_grade_id' => 3
       ],
       [
         'job_title' => 'مشاور قانوني',
         'next_job_title_id' => 4,
-        'career_stage_id' => 4
+        'job_grade_id' => 4
       ],
       [
         'job_title' => 'مشاور قانوني مساعد',
         'next_job_title_id' => 5,
-        'career_stage_id' => 5
+        'job_grade_id' => 5
       ],
       [
         'job_title' => 'قانوني',
         'next_job_title_id' => 6,
-        'career_stage_id' => 6
+        'job_grade_id' => 6
       ],
       [
         'job_title' => 'معاون قانوني',
         'next_job_title_id' => 7,
-        'career_stage_id' => 7
+        'job_grade_id' => 7
       ],
     ];
     foreach ($jobtitles as $jobtitle) {
